61a9c563b4733ebcf7ccdb75eaf88cfa1bb3500d
Juncheng ZHOU <157292@supinfo.com>
2018-12-27 09:10:39 +0100

    support css and scss and images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
-module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-
+const withImages = require('next-images');
+module.exports = withImages(
+{
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.(css|scss)/,
+      loader: 'emit-file-loader',
+      options: {name: 'dist/[path][name].[ext]'}
+    },
+    {
+      test: /\.css$/,
+      loader: 'babel-loader!raw-loader'
+    },
+    {
+      test: /\.scss$/,
+      loader: 'babel-loader!raw-loader!sass-loader'
+    },
+    );
+    return config;
+  },
+  webpackDevMiddleware: config => {
+    // Perform customizations to webpack dev middleware config
+    // Important: return the modified config
     return config
   }
-}
+  //cssModules: true
+})
