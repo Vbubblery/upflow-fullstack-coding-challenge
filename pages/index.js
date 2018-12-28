e5266c85309a3702f7451b5318fae0b3b231d4b8
Juncheng ZHOU <157292@supinfo.com>
2018-12-28 04:04:53 +0100

    added csv reader library

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Layout from '../components/Layout';
 
 // components
 import Grid from '../components/Grid';
+import CSVReader from "react-csv-reader";
 
 const tableHeader = ["sepal_length", "sepal_width", "petal_length", "petal_width","species"];
 
@@ -22,9 +23,13 @@ const tableData = [["5.1","3.5","1.4","0.2","Iris-setosa"],
                    ["4.7","3.2","1.3","0.2","Iris-setosa"],
                    ["4.6","3.1","1.5","0.2","Iris-setosa"],]
 
+const handleForce = data => console.log(data);
 
 const Index = () => (
   <Layout>
+    <CSVReader
+      onFileLoaded={handleForce}
+    />
     <Grid tableHeader={tableHeader} tableData={tableData} />
   </Layout>
 )
