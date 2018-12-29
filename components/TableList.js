89219ac0485f816b067546907e5bd89a65156f16
Vbubblery <157292@supinfo.com>
2018-12-29 20:33:00 +0100

    reset

--- components/TableList.js
+++ components/TableList.js
@@ -7,7 +7,7 @@ import {Button} from '@material-ui/core';
 
 // redux connect
 import {connect} from 'react-redux';
-import { initGrid } from '../store'
+import {initGrid, resetGrid } from '../store'
 
 class TableList extends React.Component{
   constructor(props){
@@ -20,25 +20,33 @@ class TableList extends React.Component{
   componentWillUnmount () {}
 
   handleForce = csv => {
-    const {dispatch} = this.props
+    const {dispatch} = this.props;
     const header = csv.shift();
     dispatch(initGrid({data:{header:header,body:csv}}))
+  }
+
+  reset = (e) => {
+    const {dispatch} = this.props;
+    dispatch(resetGrid());
   }
 
   render(){
     return(
       <>
-      <CSVReader
-        onFileLoaded={this.handleForce}
-        inputStyle={{display: 'none'}}
-        inputId="csv-reader"
-      />
-      <label htmlFor="csv-reader">
-        <Button variant="contained" component="span">
-          Upload CSV
+        <CSVReader
+          onFileLoaded={this.handleForce}
+          inputStyle={{display: 'none'}}
+          inputId="csv-reader"
+        />
+        <label htmlFor="csv-reader">
+          <Button variant="contained" component="span">
+            Upload CSV
+          </Button>
+        </label>
+        <Button variant="contained" component="span" onClick={this.reset} >
+          Reset
         </Button>
-      </label>
-      <Grid tableHeader={this.props.gridPage.data.header} tableData={this.props.gridPage.data.body} />
+        <Grid tableHeader={this.props.gridPage.data.header} tableData={this.props.gridPage.data.body} />
       </>
     )
   }
